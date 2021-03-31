f076817e516806946f36dd0cb7e31acf6d9795de
Arbaaz <az16aan@herts.ac.uk>
2021-03-31 18:43:22 +0100

    added physician class extends users

---
+++ src/physiotherapybooking/Physician.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package physiotherapybooking;
+
+/**
+ *
+ * @author Arbaaz
+ */
+public class Physician extends User{
+    // Consultation hours
+    // expertise
+    public Physician(String fullName, String address, String telephone){
+        super(fullName, address, telephone);
+        
+    }
+}
