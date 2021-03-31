f44b6da9b3241562c9fb173391bb38b948b40622
Arbaaz <az16aan@herts.ac.uk>
2021-03-31 23:44:11 +0100

    added and tried out LocalDateTime to potentially use for consultation hours.

--- src/physiotherapybooking/Physician.java
+++ src/physiotherapybooking/Physician.java
@@ -4,16 +4,19 @@
  * and open the template in the editor.
  */
 package physiotherapybooking;
-
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 /**
  *
  * @author Arbaaz
  */
 public class Physician extends User{
-    // Consultation hours
+    LocalDateTime[] ConsultationHours;
     // expertise
+    
     public Physician(String fullName, String address, String telephone){
         super(fullName, address, telephone);
+        ConsultationHours = new LocalDateTime[10];
         
     }
 }
---
+++ src/physiotherapybooking/Tester1.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package physiotherapybooking;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.Month;
+/**
+ *
+ * @author Arbaaz
+ */
+public class Tester1 {
+    public static void main (String[] args){
+        LocalDateTime booking = LocalDateTime.of(2020, Month.MARCH, 01, 13, 0);
+        System.out.println(booking);
+        
+        LocalDateTime arrive = LocalDateTime.of(2020, Month.MARCH, 01, 13, 29);
+        if(arrive.isAfter(booking) && arrive.isBefore(booking.plusMinutes(30))) {
+            System.out.println("arrived");
+        }
+            
+        
+    }
+}
