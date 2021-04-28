d4165bf3f3b2e8687cc522e7b89d904d7d8e551f
Arbaaz <az16aan@herts.ac.uk>
2021-04-28 15:44:15 +0100

    can now fully book treatments, user does not exist also works correctly

--- src/physiotherapybooking/BookingController.java
+++ src/physiotherapybooking/BookingController.java
@@ -16,6 +16,8 @@ import java.util.Scanner;
 public class BookingController {
 
     private Scanner input = new Scanner(System.in);
+    private Scanner sc1 = new Scanner(System.in);
+    private Scanner sc2 = new Scanner(System.in);
     private Patient sysUser;
 
     private PhysicianList listOfPhysician = new PhysicianList();
@@ -24,16 +26,10 @@ public class BookingController {
     private BookingList bookingList = new BookingList();
 
     public BookingController() {
-        //boolean sysActive = true;
-        //System.out.println("Welcome to The Physiotherapy & Sports Injury Centre (PSIC) Booking system");
-        int option;
-        //String usersInput = "";
 
-        Scanner sc1 = new Scanner(System.in);
-        Scanner sc2 = new Scanner(System.in);
+        int option;
+
         System.out.println("Welcome to The Physiotherapy & Sports Injury Centre (PSIC) Booking system");
-//        System.out.println("=========================================================================");
-//        System.out.println("Enter: ( 0 )to log out\nEnter: ( 1 ) to login\nEnter: ( 2 ) Sign up\nEnter: ( 3 ) Continue as Visitor");
 
         //option = sc1.nextInt();
         do {
@@ -48,96 +44,16 @@ public class BookingController {
                 case 1:
                     login();
                     break;
-//                    String usersInput = "";
-//                    do {
-//                        System.out.println("please enter user ID");
-//                        usersInput = sc2.nextLine();
-//                    } while (usersInput.isEmpty());
-//                    if (listOfPatients.patientExists(usersInput)) {
-//                        sysUser = listOfPatients.getPatientByID(usersInput);
-//                        System.out.println("Welcome " + sysUser.getFullName());
-//                        usersInput = null;
-//                        int logOption;
-//
-//                        /////////////////////////////////////////////////////////////////
-//                        // search for physician via name; view bookings;
-//                        //logOption = sc1.nextInt();
-////                        do {
-////                            System.out.println("Enter: ( 1 ) Search for Physician by name\nEnter: ( 2 )");
-////
-////                            logOption = sc1.nextInt();
-////                            switch (logOption) {
-////                                case 1:
-////                                    String enteredName = "";
-////                                    do {
-////                                        System.out.println("Enter a physicians name: ");
-////                                        enteredName = input.nextLine();
-////                                    } while (enteredName.isEmpty());
-////
-////                                    if (listOfPhysician.physicianExists(enteredName)) {
-////                                        System.out.println(listOfPhysician.getAPhysicianByName(enteredName));
-////                                        do {
-////                                            System.out.println("Would you like to select a appointment? (Y/N)");
-////                                            usersInput = input.nextLine();
-////                                        } while (usersInput.isEmpty());
-////
-////                                        if (usersInput.equalsIgnoreCase("y")) {
-////                                            //select time
-////                                            //Physician phys = new Physician(listOfPhysician.getAPhysicianByName(usersInput)); 
-////                                            // ask for year month time
-////                                            System.out.println();
-////                                            usersInput = "";
-////
-////                                            do {
-////                                                System.out.println("selct a date");
-////                                                usersInput = input.nextLine();
-////                                            } while (usersInput.isEmpty());
-////                                            int selectedDate = Integer.parseInt(usersInput);
-////
-////                                            Physician phys = listOfPhysician.getAPhysicianByName(enteredName);
-////                                            LocalDateTime selected = (listOfPhysician.getAPhysicianByName(enteredName).selectTime(selectedDate));
-////
-////                                            first:
-////                                            for (int i = 0; i <= listOfRooms.getSize(); i++) {
-////                                                Room room = listOfRooms.getRoom(i);
-////                                                Treatment tr = new Treatment("", room, selected, phys, sysUser);
-////
-////                                                Booking book = new Booking(tr);
-////                                                String mesg = bookingList.addBooking(book);
-////                                                if (mesg.equalsIgnoreCase("Booking complete")) {
-////                                                    System.out.println(mesg);
-////                                                    break first;
-////                                                } else if (i == listOfRooms.getSize()) {
-////                                                    System.out.println(mesg);
-////                                                    break first;
-////                                                }
-////                                            }
-////
-////                                            //Room room = new Room("");
-////                                            //Treatment tr = new Treatment("",room,selected,phys, sysUser);
-////                                        }
-////                                        /////////////////////////////////////////////////////////////////
-////
-////                                    } else {
-////                                        System.out.println("USER DOES NOT EXIST");
-////                                    }
-////                                    option = -1;
-////                                    break;
-////                            }
-////                        } while (logOption != 0);
-//                    }
 
-                /////////////////////////////////////////////////////////////////////////////////////////////////
                 case 2:
                     signUp();
                     break;
-                    
+
                 case 3:
                     break;
 
             }
         } while (option != 0);
-        //System.out.println("Thank you for using the system");
     }
 
     private void signUp() {
@@ -166,7 +82,6 @@ public class BookingController {
         //return option = -1;
     }
 
-    //System.out.println("Thank you for using the system");
     private void exit() {
         System.out.println("Thank you for using the system");
         System.exit(0);
@@ -174,9 +89,7 @@ public class BookingController {
 
     private void login() {
         String usersInput = "";
-        //int option;
-        Scanner sc1 = new Scanner(System.in);
-        Scanner sc2 = new Scanner(System.in);
+
         do {
             System.out.println("please enter user ID");
             usersInput = sc2.nextLine();
@@ -187,11 +100,8 @@ public class BookingController {
             usersInput = null;
             int logOption;
 
-            /////////////////////////////////////////////////////////////////
-            // search for physician via name; view bookings;
-            //logOption = sc1.nextInt();
             do {
-                System.out.println("Enter: ( 1 ) Search for Physician by name\nEnter: ( 2 )");
+                System.out.println("Enter: ( 0 ) Log out\nEnter: ( 1 ) Search for Physician by name\nEnter: ( 2 )");
 
                 logOption = sc1.nextInt();
                 switch (logOption) {
@@ -205,30 +115,37 @@ public class BookingController {
                         if (listOfPhysician.physicianExists(enteredName)) {
                             System.out.println(listOfPhysician.getAPhysicianByName(enteredName));
                             do {
-                                System.out.println("Would you like to select a appointment? (Y/N)");
+                                System.out.println("Would you like to make a treatment appointment? (Y/N)");
                                 usersInput = input.nextLine();
                             } while (usersInput.isEmpty());
 
                             if (usersInput.equalsIgnoreCase("y")) {
-                                //select time
-                                //Physician phys = new Physician(listOfPhysician.getAPhysicianByName(usersInput)); 
-                                // ask for year month time
+                                //usersInput = "";
+                                Physician phys = listOfPhysician.getAPhysicianByName(enteredName);
                                 System.out.println();
+
+                                do {
+                                    System.out.println("Enter a treatment");
+                                    System.out.println(phys.getAllExpertise());
+                                    usersInput = input.nextLine();
+                                } while (usersInput.isEmpty());
+                                String treatment = phys.getExpertise(usersInput);
+
                                 usersInput = "";
 
                                 do {
                                     System.out.println("selct a date");
+                                    System.out.println(phys.getAppointmentHours());
                                     usersInput = input.nextLine();
                                 } while (usersInput.isEmpty());
                                 int selectedDate = Integer.parseInt(usersInput);
 
-                                Physician phys = listOfPhysician.getAPhysicianByName(enteredName);
-                                LocalDateTime selected = (listOfPhysician.getAPhysicianByName(enteredName).selectTime(selectedDate));
+                                LocalDateTime selected = (phys.selectTime(selectedDate));
 
                                 first:
                                 for (int i = 0; i <= listOfRooms.getSize(); i++) {
                                     Room room = listOfRooms.getRoom(i);
-                                    Treatment tr = new Treatment("", room, selected, phys, sysUser);
+                                    Treatment tr = new Treatment(treatment, room, selected, phys, sysUser);
 
                                     Booking book = new Booking(tr);
                                     String mesg = bookingList.addBooking(book);
@@ -241,19 +158,20 @@ public class BookingController {
                                     }
                                 }
 
-                                //Room room = new Room("");
-                                //Treatment tr = new Treatment("",room,selected,phys, sysUser);
                             }
-                            /////////////////////////////////////////////////////////////////
 
-                        } else {
-                            System.out.println("USER DOES NOT EXIST");
                         }
-                        
-                        
+
+                    case 2:
+                        // view my bookings -> change/cancel bookings
+
+                        break;
                 }
             } while (logOption != 0);
+        } else {
+            System.out.println("USER DOES NOT EXIST");
         }
         //return option = -1;
+        sysUser = null;
     }
 }
